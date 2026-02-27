41773b4d91b1a9527d8e68cda7556c0e09337844
Jin Zhou <jzhou24@condorfe.crc.nd.edu>
2026-02-27 14:35:37 -0500

    update

--- taskvine_report/static/js/modules/task_completion_percentiles.js
+++ taskvine_report/static/js/modules/task_completion_percentiles.js
@@ -3,19 +3,19 @@ import { BaseModule } from './base.js';
 export class TaskCompletionPercentilesModule extends BaseModule {
     constructor(id, title, api_url) {
         super(id, title, api_url);
-        this.setBottomScaleType('band');
-        this.setLeftScaleType('linear');
+        this.setBottomScaleType('linear');
+        this.setLeftScaleType('band');
     }
 
     plot() {
         if (!this.data) return;
 
-        const xWidth = this.bottomScale.bandwidth() * 0.8;
-        const yFormatter = eval(this.data['y_tick_formatter']);
+        const yHeight = this.leftScale.bandwidth() * 0.8;
+        const xFormatter = eval(this.data['x_tick_formatter']);
 
-        this.data['points'].forEach(([percentile, time]) => {
-            const innerHTML = `Percentile: ${percentile}%<br>Time: ${yFormatter(time)}`;
-            this.plotVerticalRect(percentile, xWidth, time, 'steelblue', 1, innerHTML);
+        this.data['points'].forEach(([time, percentile]) => {
+            const innerHTML = `Time: ${xFormatter(time)}<br>Percentile: ${percentile}%`;
+            this.plotHorizontalRect(0, time, percentile, yHeight, 'steelblue', 1, innerHTML);
         });
     }
 } 
